82c9a90ffe880b9092f90a42d8113c0b334970ef
Bladimir Nunez <Nunez.Bladimir@yahoo.com>
2022-04-28 13:09:46 -0400

    added button with functioning fortunes

--- MagicEightBall/ContentView.swift
+++ MagicEightBall/ContentView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentFortune = "Click to learn your fate"
     var body: some View {
-        Text("Hello Bladimir Nunez")
+        VStack(alignment: .leading) {
+            Button("Tell me my fortune") {
+                
+               let choice = Int.random(in: 1...3)
+                
+                if(choice == 1){
+                    currentFortune = "yes"
+                }
+                if(choice == 2){
+                    currentFortune = "No"
+                }
+                if(choice == 3){
+                    currentFortune = "Maybe"
+                }
+            }
+            
+            
+            
+            Text(currentFortune)
+                .font(.largeTitle)
+                .foregroundColor(Color.yellow)
             .padding()
+        }
     }
+    
+    
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
