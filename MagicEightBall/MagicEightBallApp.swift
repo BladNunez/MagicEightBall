c813548b684d2b269e1ef5062c0330645486934b
Bladimir Nunez <Nunez.Bladimir@yahoo.com>
2022-04-28 12:06:19 -0400

    Initial Commit

---
+++ MagicEightBall/MagicEightBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicEightBallApp.swift
+//  MagicEightBall
+//
+//  Created by Bladimir Nunez on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MagicEightBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
